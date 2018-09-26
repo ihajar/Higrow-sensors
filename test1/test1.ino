b3e6be7fcd6758d0c3fa595e7df46df4d4f4e295
hajar-65 <hadjero.hamaidi@gmail.com>
2018-09-26 19:48:43 +0900

    Add files via upload

---
+++ test1/test1.ino
@@ -0,0 +1,95 @@
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
+
+// Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "a1e527e8523040db8adf06fde580fe13";
+
+// WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Buffalo-G-38D3";
+char pass[] = "n7pws65ixfb7f";
+
+//char ssid[] = "KIC_LOCAL_G";
+//char pass[] = "KICKICKICKICKIC";
+
+//char ssid[] = "AndroidAPms";
+//char pass[] = "ttw-12345";
+
+#define DHTPIN 22          // What digital pin is connected to
+const int soilpin = 32;
+const int POWER_PIN = 34;
+const int LIGHT_PIN = 33;
+
+#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  int soilmoisture = analogRead(soilpin);
+  int lightlevel = analogRead(LIGHT_PIN);
+
+//  waterlevel = map(waterlevel, 0, 4095, 0, 1023);
+//  waterlevel = constrain(waterlevel, 0, 1023);
+//  lightlevel = map(lightlevel, 0, 4095, 0, 1023);
+//  lightlevel = constrain(lightlevel, 0, 1023);
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  if (isnan(t)) {
+    return;
+  }
+  //if (t > 25) {
+  //  Blynk.notify(String("Teperature is too high: ") + t);
+  //}
+  // send any value at any time.
+  // No more that 10 values per second.
+  Blynk.virtualWrite(V5, h);
+  Blynk.virtualWrite(V6, t);
+  Blynk.virtualWrite(V7, soilmoisture);
+  Blynk.virtualWrite(V8, lightlevel);
+}
+
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // Specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 8442);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8442);
+
+  dht.begin();
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+
+
